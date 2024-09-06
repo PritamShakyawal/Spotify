c874120aa0a53cf7aa4db9c84379c4dca9fe1ded
Pritam <95740918+PritamShakyawal@users.noreply.github.com>
2024-09-06 20:07:50 +0530

    Update App.jsx

--- spotify-admin/src/App.jsx
+++ spotify-admin/src/App.jsx
@@ -10,8 +10,8 @@ import Sidebar from './components/Sidebar';
 import Navbar from './components/Navbar';
 
 
-// export const url = 'http://localhost:4000'
-export const url = 'http://spotify-eta-drab.vercel.app'
+export const url = 'https://spotify-backend-2ycj.onrender.com'
+
 
 const App = ()=> {
   return (
